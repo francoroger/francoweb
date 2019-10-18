a005b13a64df99635a6fbcffb08cfc08e68ed2aa
William <wr.sant@gmail.com>
2019-10-18 11:31:28 -0300

    Alteração ordenação 2

--- app/Http/Controllers/CheckListCatalogacaoController.php
+++ app/Http/Controllers/CheckListCatalogacaoController.php
@@ -97,7 +97,7 @@ class CheckListCatalogacaoController extends Controller
     $catalogacao = Catalogacao::findOrFail($id);
 
     $itens = $catalogacao->itens->sortBy(function($item) {
-      return sprintf('%-12s%s', $item->descricao_produto, $item->fornecedor->nome ?? '', $item->status_check);
+      return sprintf('%-12s%s', $item->descricao_produto, $item->status_check, $item->fornecedor->nome ?? '');
     });
 
     $produtos = $catalogacao->itens->unique('produto.descricao')->sortBy('produto.descricao')->pluck('produto.descricao', 'produto.id');
