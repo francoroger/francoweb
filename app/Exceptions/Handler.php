a54d6ce5e86dd5a0cb34a54974fc33f12baa0d7d
francoroger <167598044+francoroger@users.noreply.github.com>
2024-12-28 11:25:49 -0300

    fix: Resolvido conflito no Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,7 @@
 
 namespace App\Exceptions;
 
-<<<<<<< HEAD
 use Throwable;
-=======
-use Exception;
->>>>>>> 7e5b37cb9f83395d9e052fa4aaa0900d02db0493
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -14,11 +10,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-<<<<<<< HEAD
      * @var array<int, class-string<Throwable>>
-=======
-     * @var array
->>>>>>> 7e5b37cb9f83395d9e052fa4aaa0900d02db0493
      */
     protected $dontReport = [
         //
@@ -27,22 +19,15 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-<<<<<<< HEAD
      * @var array<int, string>
      */
     protected $dontFlash = [
         'current_password',
-=======
-     * @var array
-     */
-    protected $dontFlash = [
->>>>>>> 7e5b37cb9f83395d9e052fa4aaa0900d02db0493
         'password',
         'password_confirmation',
     ];
 
     /**
-<<<<<<< HEAD
      * Register the exception handling callbacks for the application.
      *
      * @return void
@@ -52,27 +37,5 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-=======
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
->>>>>>> 7e5b37cb9f83395d9e052fa4aaa0900d02db0493
     }
 }
