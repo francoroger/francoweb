7d38facb0e270a97717c6edfe7da682548fcd42b
francoroger <167598044+francoroger@users.noreply.github.com>
2024-12-29 11:53:12 -0300

    config: Atualizações nas configurações do sistema
    - Atualizado TrustProxies middleware
    - Ajustes nas configurações gerais da aplicação

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
-     * @var array|string
+     * @var array|string|null
      */
     protected $proxies;
 
@@ -19,14 +19,10 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-<<<<<<< HEAD
-    protected $headers = 
+    protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
-=======
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
->>>>>>> 7e5b37cb9f83395d9e052fa4aaa0900d02db0493
 }
